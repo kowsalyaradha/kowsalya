db9dcc0e4ddade55345dcfa644d417ae036cfa66
kowsalyaradha <42797493+kowsalyaradha@users.noreply.github.com>
2018-08-29 03:51:59 -0700

    Update vc.c

--- vc.c
+++ vc.c
@@ -1,20 +1,21 @@
-
-#include <stdio.h>
-
+#include<stdio.h>
 void main()
 {
-char ch;
-scanf("%c",&ch);
-if((ch>='a'&&ch<='z')&&(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'))
-{
-printf("Vowel");
+  char ch;
+  scanf("%c",&ch);
+  if((ch>='a'&&ch<='z')&&(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'))
+  {
+    printf("Vowel");
+  }
+  else if((ch>='a'&&ch<='z')&&(ch!='a'||ch!='e'||ch!='i'||ch!='o'||ch!='u'))
+  {
+    printf("Consonant);
+  }
+  else
+  {
+    printf("invalid");
+  }
 }
-else if((ch>='a'&&ch<='z')&&(ch!='a'||ch!='e'||ch!='i'||ch!='o'||ch!='u'))
-{
-printf("Consonant");
-}
-else
-{
-printf("invalid");
-}
-}
+  
+
+     
