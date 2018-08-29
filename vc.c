ac3aee42fdf873b7117c476a98635802165a3d75
kowsalyaradha <42797493+kowsalyaradha@users.noreply.github.com>
2018-08-29 03:10:56 -0700

    Create vc.c

---
+++ vc.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+printf("Vowel");
+}
+else if(ch!='a'||ch!='e'||ch!='i'||ch!='o'||ch!='u')
+{
+printf("Consonant");
+}
+else
+{
+printf("invalid");
+}
+}
