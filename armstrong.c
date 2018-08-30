972cdff7e74e1b975aaa8c346c15f3560dbe3a04
kowsalyaradha <42797493+kowsalyaradha@users.noreply.github.com>
2018-08-30 07:24:41 -0700

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int num,on,ar=0;n=0;rem;
+scanf("%d",&num);
+on=num;
+while(on!=0)
+{
+on/=10;
+++n;
+}
+on=num;
+while(on!=0)
+{
+rem=on%10;
+ar+=pow(rem,n);
+on/=10;
+}
+if(on==ar)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
