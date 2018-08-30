80bed183376dec6dfbbac6077853fa1665e0b840
kowsalyaradha <42797493+kowsalyaradha@users.noreply.github.com>
2018-08-30 10:33:38 -0700

    Update NK.c

--- NK.c
+++ NK.c
@@ -3,12 +3,12 @@ void main()
 {
 int i,n,k,d,a[10],sum=0;
 scanf("%d%d",&n,&k);
-for(i=1;i<=n;i++)
+for(i=0;i<n;i++)
 {
-scanf("%d",d);
+scanf("%d",&d);
   a[i]=d;
 }
-for(j=1;j<=k;j++)
+for(i=0;i<k;j++)
 {
 sum+=a[i];
 }
