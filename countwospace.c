4d649afa912b52b3f4e44587554e7d7b0cef7c04
kowsalyaradha <42797493+kowsalyaradha@users.noreply.github.com>
2018-09-20 19:53:06 -0700

    Create countwospace.c

---
+++ countwospace.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int l,ans,count=0;
+char str[20];
+scanf("%[^\n]%*c",str);
+l=strlen(str);
+for(i=0;i<20;i++)
+{
+if(str[i]=='')
+{
+count++;
+}
+}
+ans=l-count;
+printf("\n%d",ans);
