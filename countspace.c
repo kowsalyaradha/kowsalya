4a0632510cec585d9514cee180fdec8d5ecb7ec2
kowsalyaradha <42797493+kowsalyaradha@users.noreply.github.com>
2018-09-20 20:16:57 -0700

    Create countspace.c

---
+++ countspace.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i,count=0;
+char str[20];
+scanf("%[^\n]%*c",str);
+for(i=0;i<20;i++)
+{
+if(str[i]==' ')
+{
+count++;
+}
+}
+printf("\n%d",count);
+return 0;
+}
